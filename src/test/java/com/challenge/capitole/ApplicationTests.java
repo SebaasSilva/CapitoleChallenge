becd87ae8d507641299dfbe48aa3468982e11165
Sebastian Silva <sebastian.silva@mercadolibre.com.co>
2023-05-25 20:19:53 -0500

    Technical challenge Capitole

---
+++ src/test/java/com/challenge/capitole/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.capitole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
